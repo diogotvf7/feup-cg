5bf54a0b524628d3f9a46c0d7bfe0f65fe283418
João Torre Pereira <up202108848@fe.up.pt>
2024-04-07 12:04:05 +0100

    Fix last slice repeating vertices

--- project/MySphere.js
+++ project/MySphere.js
@@ -31,12 +31,12 @@ export class MySphere extends CGFobject {
 
   initBuffers() {
     this.vertices = [
-      0, 1, 0,  // Top vertex
-      0, -1, 0  // Bottom vertex
+      0, this.radius, 0,  // Top vertex
+      0, -this.radius, 0  // Bottom vertex
     ];
     this.normals = [
-      ...this.normal(0, -1, 0),  // Top normal vertex
-      ...this.normal(0, 1, 0),  // Bottom normal vertex
+      ...this.normal(0, 1, 0),  // Top normal vertex
+      ...this.normal(0, -1, 0),  // Bottom normal vertex
     ];
     this.indices = [];
     this.texCoords = [
@@ -48,7 +48,7 @@ export class MySphere extends CGFobject {
 
     // Vertices and normals
     for (let i=0; i<this.stacks; i++){
-      for(let j=0; j<this.slices; j++){
+      for(let j=0; j<this.slices+1; j++){
         const stack_angle = Math.PI/2 - Math.PI*i/this.stacks;
         const slice_angle = j*slice_angle_increment;
 
@@ -63,21 +63,21 @@ export class MySphere extends CGFobject {
     }
 
     // Top Indices (Triangulation)
-    for (let i=1; i<this.slices; i++){
+    for (let i=0; i<this.slices; i++){
       const current_vertex = i
       const next_vertex = (i+1)%this.slices;
       this.indices.push(0, current_vertex, next_vertex);
     }
     
     // Middle Indices
-    for (let i=0; i<this.stacks-1; i++){
-      for (let j=0; j<this.slices; j++){
-        const current_vertex = i*this.slices + j + 2;
-        const next_vertex = i*this.slices + (j+1)%this.slices + 2;
-
-        const next_stack_vertex = (i+1)*this.slices + j + 2;
-        const next_stack_next_vertex = (i+1)*this.slices + (j+1)%this.slices + 2;
-
+    for (let i=0; i<this.stacks-1; i++) {
+      for (let j=0; j<this.slices; j++) {
+        const current_vertex = i * (this.slices + 1) + j + 2;
+        const next_vertex = i * (this.slices + 1) + (j + 1) % (this.slices + 1) + 2;
+    
+        const next_stack_vertex = (i + 1) * (this.slices + 1) + j + 2;
+        const next_stack_next_vertex = (i + 1) * (this.slices + 1) + (j + 1) % (this.slices + 1) + 2;
+    
         this.indices.push(current_vertex, next_stack_vertex, next_vertex);
         this.indices.push(next_vertex, next_stack_vertex, next_stack_next_vertex);
       }
